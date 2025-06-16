c8eac5a8bc19c4f66706a81c7412fe4ea5724b96
Dhean Nurwanti <dheanurwanti24@student.unibi.ac.id>
2025-06-16 21:28:35 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -6,46 +6,28 @@ export default {
       items: [
         {
           id: 1,
-          name: 'Personal Website',
-          imageUrl: 'portfolio_v2',
-          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
-          tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/Portofolio-v2',
-          demo: 'https://bagasrakha.netlify.app/'
+          name: 'Smm Nesia',
+          imageUrl: '20241125_062029',
+          status: 'Smm Nesia adalah project panel smm yang simpel dan No Login yang terintegrasi ke payment gateway',
+          tech: 'Codeigniter 3, Bootstrap, Vanila CSS, Vanila JS',
+          github: 'null',
+          demo: 'null'
         },
         {
           id: 2,
-          name: 'Sahabat Muslim',
-          imageUrl: 'sahabatmuslim',
-          status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
-          tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/muslim-companion',
-          demo: 'https://sahabat-muslim.netlify.app/'
+          name: 'Frey App',
+          imageUrl: '20241125_062516',
+          status: 'Frey App adalah sebuah website penyedia api tools serba guna.'',
+          tech: 'Codeigniter 3, Bootstrap',
+          github: 'null',
+          demo: 'null'
         },
         {
           id: 3,
-          name: 'SISTEKDES',
-          imageUrl: 'sistekdes',
-          status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
-          tech: 'Laravel 9, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 4,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
-          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 5,
-          name: 'SP2SE',
-          imageUrl: 'sp2se',
-          status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
-          tech: 'Codeigniter 3, Bootstrap',
+          name: 'Anime Streming',
+          imageUrl: 'animestrem',
+          status: 'Streming anime sub indo gratis',
+          tech: 'ReactJs',
           github: 'null',
           demo: 'null'
         }
